9647b9596c766a95dee5b966c7d97587538584c7
German Gizatullin <snolldors@inbox.ru>
2023-03-21 23:35:07 +0500

    Initial commit

---
+++ src/test/java/ru/netology/web/AppOrderTest.java
@@ -0,0 +1,51 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestV1() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петров Владимир");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79178899808");
+        driver.findElement(By.cssSelector("[data-test-id=agreement")).click();
+        driver.findElement(By.className("button__text")).click();
+        String expected = " Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals(expected,actual);
+
+
+
+    }
+
+}
+
